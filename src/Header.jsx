0fc16333b4049ad3ea7af1c3abf12cf6950f35d6
japhethh <boknoy32130@gmail.com>
2024-06-25 15:47:52 +0800

    =

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,22 @@
 import React, { useState } from "react";
+import { messages, conversation } from "./assets/data";
 
 const Header = () => {
+  // Opening the List peoples chats
   const [openMessenger, setOpenMessenger] = useState(false);
+  // end
+
+  // List peoples hover
   const [hoverListChat, setHoverListChat] = useState(-1);
+  // end
+
   const [hoverListCondition, setHoverListCondition] = useState(false);
   const [openConversation, setOpenCoversation] = useState(true);
+
+  const openHandleChat = () => {
+    setOpenCoversation(true);
+    setOpenMessenger(false);
+  };
 
   const handleMouseEnter = (index) => {
     setHoverListCondition(true);
@@ -15,85 +27,6 @@ const Header = () => {
     setHoverListChat(null);
     setHoverListCondition(false);
   };
-  const messages = [
-    {
-      id: 1,
-      name: "Adrian Barrios",
-      nickname: "Underdog",
-      profile:
-        "https://scontent.fcrk1-4.fna.fbcdn.net/v/t39.30808-1/342805416_6430692393627804_7089258403426895782_n.jpg?stp=dst-jpg_p100x100&_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeG7ztbAylG1EyV1_UXH8HyoLsM3d7qxNiAuwzd3urE2IObxzoQ7bkUiSo0Btdi3G45IyOY3PjehkPy187KWTzUI&_nc_ohc=CrwY1611DHEAb5BXT9H&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.fcrk1-4.fna&oh=00_AfC1f3-35BZJGcEsCXLsTr1ekepY9Hz1S9Y4EvRrjTvbNg&oe=66176AF9",
-      chat: "Dami mo nangang electricfan",
-      // more
-    },
-    {
-      id: 2,
-      name: "Jobert Dela Cruz",
-      nickname: "Jobert Dela Cruz",
-      profile:
-        "https://scontent.fcrk1-5.fna.fbcdn.net/v/t39.30808-1/433144291_2111893395837094_5241777717003793999_n.jpg?stp=dst-jpg_p100x100&_nc_cat=104&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeHXrdOIQi8U1LvDkzXA6JyG7ABShiAUAWvsAFKGIBQBa1eDMolX6Z7JaAw10L_rvs_OR8ydxx1WMvqpuaS2TOx2&_nc_ohc=g8P7FkBul2YAb7udk-X&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.fcrk1-5.fna&oh=00_AfBXgUuR6toBVyrwiEv31XIFAwuVWVHLNzCoSr1GpMP65w&oe=661777A1",
-      chat: "Gago HAHHAH YUNG MUKA KO DYAN 😭",
-      // more
-    },
-    {
-      id: 3,
-      name: "Jobert Dela Cruz",
-      nickname: "Jobert Dela Cruz",
-      profile:
-        "https://scontent.fcrk1-5.fna.fbcdn.net/v/t39.30808-1/433144291_2111893395837094_5241777717003793999_n.jpg?stp=dst-jpg_p100x100&_nc_cat=104&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeHXrdOIQi8U1LvDkzXA6JyG7ABShiAUAWvsAFKGIBQBa1eDMolX6Z7JaAw10L_rvs_OR8ydxx1WMvqpuaS2TOx2&_nc_ohc=g8P7FkBul2YAb7udk-X&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.fcrk1-5.fna&oh=00_AfBXgUuR6toBVyrwiEv31XIFAwuVWVHLNzCoSr1GpMP65w&oe=661777A1",
-      chat: "Gago HAHHAH YUNG MUKA KO DYAN 😭",
-      // more
-    },
-    {
-      id: 4,
-      name: "Jobert Dela Cruz",
-      nickname: "Jobert Dela Cruz",
-      profile:
-        "https://scontent.fcrk1-5.fna.fbcdn.net/v/t39.30808-1/433144291_2111893395837094_5241777717003793999_n.jpg?stp=dst-jpg_p100x100&_nc_cat=104&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeHXrdOIQi8U1LvDkzXA6JyG7ABShiAUAWvsAFKGIBQBa1eDMolX6Z7JaAw10L_rvs_OR8ydxx1WMvqpuaS2TOx2&_nc_ohc=g8P7FkBul2YAb7udk-X&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.fcrk1-5.fna&oh=00_AfBXgUuR6toBVyrwiEv31XIFAwuVWVHLNzCoSr1GpMP65w&oe=661777A1",
-      chat: "Gago HAHHAH YUNG MUKA KO DYAN 😭",
-      // more
-    },
-    {
-      id: 6,
-      name: "Adrian Barrios",
-      nickname: "Underdog",
-      profile:
-        "https://scontent.fcrk1-4.fna.fbcdn.net/v/t39.30808-1/342805416_6430692393627804_7089258403426895782_n.jpg?stp=dst-jpg_p100x100&_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeG7ztbAylG1EyV1_UXH8HyoLsM3d7qxNiAuwzd3urE2IObxzoQ7bkUiSo0Btdi3G45IyOY3PjehkPy187KWTzUI&_nc_ohc=CrwY1611DHEAb5BXT9H&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.fcrk1-4.fna&oh=00_AfC1f3-35BZJGcEsCXLsTr1ekepY9Hz1S9Y4EvRrjTvbNg&oe=66176AF9",
-      chat: "Dami mo nangang electricfan",
-      // more
-    },
-  ];
-
-  const latestConvo = () => {
-    conversation
-  }
-
-  const conversation = [
-    {
-      id: 1,
-      personName:"Adrey Barrios",
-      personProfile:
-        "https://scontent.fcrk1-4.fna.fbcdn.net/v/t39.30808-1/342805416_6430692393627804_7089258403426895782_n.jpg?stp=dst-jpg_p100x100&_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeG7ztbAylG1EyV1_UXH8HyoLsM3d7qxNiAuwzd3urE2IObxzoQ7bkUiSo0Btdi3G45IyOY3PjehkPy187KWTzUI&_nc_ohc=mTdJwZaqlisAb5E4R9X&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.fcrk1-4.fna&oh=00_AfC8XmkvEiBKoXjCPs-Niuk4kd0yX9v3-Un1jzKNGag-IA&oe=66188439",
-      personConversation: "",
-      user: "https://scontent.fcrk1-2.fna.fbcdn.net/v/t39.30808-1/423672175_3699153610408415_4085387455801009384_n.jpg?stp=cp0_dst-jpg_p86x86&_nc_cat=110&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeE_ol_DZaqAWNxJ5BfaaPWI77iDPC4fWS_vuIM8Lh9ZL65BciG9FD3fTasWn-XkrZWk3Ag6X2DGMbzZNj9MuUuv&_nc_ohc=uMkzMc-9CP0Ab5vZLAt&_nc_ht=scontent.fcrk1-2.fna&oh=00_AfArx3DBDHLDqNCuap3Bj4m27GJT5aUHB4-0saMkB_4OSA&oe=661883D0",
-      userConversation: "Oo naman",
-    },
-    {
-      id: 2,
-      personProfile:
-        "https://scontent.fcrk1-4.fna.fbcdn.net/v/t39.30808-1/342805416_6430692393627804_7089258403426895782_n.jpg?stp=dst-jpg_p100x100&_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeG7ztbAylG1EyV1_UXH8HyoLsM3d7qxNiAuwzd3urE2IObxzoQ7bkUiSo0Btdi3G45IyOY3PjehkPy187KWTzUI&_nc_ohc=mTdJwZaqlisAb5E4R9X&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.fcrk1-4.fna&oh=00_AfC8XmkvEiBKoXjCPs-Niuk4kd0yX9v3-Un1jzKNGag-IA&oe=66188439",
-      personConversation: " Jap Pogi mo talaga",
-      user: "https://scontent.fcrk1-2.fna.fbcdn.net/v/t39.30808-1/423672175_3699153610408415_4085387455801009384_n.jpg?stp=cp0_dst-jpg_p86x86&_nc_cat=110&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeE_ol_DZaqAWNxJ5BfaaPWI77iDPC4fWS_vuIM8Lh9ZL65BciG9FD3fTasWn-XkrZWk3Ag6X2DGMbzZNj9MuUuv&_nc_ohc=uMkzMc-9CP0Ab5vZLAt&_nc_ht=scontent.fcrk1-2.fna&oh=00_AfArx3DBDHLDqNCuap3Bj4m27GJT5aUHB4-0saMkB_4OSA&oe=661883D0",
-      userConversation: "hello",
-    },
-    {
-      id: 3,
-      personProfile:
-        "https://scontent.fcrk1-4.fna.fbcdn.net/v/t39.30808-1/342805416_6430692393627804_7089258403426895782_n.jpg?stp=dst-jpg_p100x100&_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeG7ztbAylG1EyV1_UXH8HyoLsM3d7qxNiAuwzd3urE2IObxzoQ7bkUiSo0Btdi3G45IyOY3PjehkPy187KWTzUI&_nc_ohc=mTdJwZaqlisAb5E4R9X&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.fcrk1-4.fna&oh=00_AfC8XmkvEiBKoXjCPs-Niuk4kd0yX9v3-Un1jzKNGag-IA&oe=66188439",
-      personConversation: "",
-      user: "https://scontent.fcrk1-2.fna.fbcdn.net/v/t39.30808-1/423672175_3699153610408415_4085387455801009384_n.jpg?stp=cp0_dst-jpg_p86x86&_nc_cat=110&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeE_ol_DZaqAWNxJ5BfaaPWI77iDPC4fWS_vuIM8Lh9ZL65BciG9FD3fTasWn-XkrZWk3Ag6X2DGMbzZNj9MuUuv&_nc_ohc=uMkzMc-9CP0Ab5vZLAt&_nc_ht=scontent.fcrk1-2.fna&oh=00_AfArx3DBDHLDqNCuap3Bj4m27GJT5aUHB4-0saMkB_4OSA&oe=661883D0",
-      userConversation: "hello",
-    },
-  ];
 
   return (
     <header>
@@ -195,11 +128,12 @@ const Header = () => {
                           key={index}
                           onMouseEnter={() => handleMouseEnter(index)}
                           onMouseLeave={() => handleMouseLeave()}
+                          onClick={() => openHandleChat()}
                           className={`${
                             hoverListChat === index && hoverListCondition
                               ? "bg-gray-100"
                               : ""
-                          } relative flex justify-start items-center gap-2 rounded-md  py-2 px-2 cursor-pointer`}
+                          } relative flex justify-start items-center gap-2 rounded-md  py-2 px-2 cursor-pointer `}
                         >
                           <div>
                             <img
@@ -297,12 +231,9 @@ const Header = () => {
             {/* card-message-body */}
             <div className="px-2 py-2 w-full">
               <ul className="overflow-y-scroll h-80 w-full">
-              <div className="header-body">
-                    <img src={conversation.personProfile[3]} alt="" />
-                </div>
+                <div className="header-body"></div>
                 {conversation.map((chat, index) => (
                   <li key={index}>
-                  
                     <div
                       className={`${
                         chat.personConversation !== "" ? "flex" : "hidden"
---
+++ src/assets/data/index.js
@@ -0,0 +1,68 @@
+export const conversation = [
+  {
+    id: 1,
+    personName:"Adrey Barrios",
+    personProfile:
+      "https://scontent.fcrk1-4.fna.fbcdn.net/v/t39.30808-1/342805416_6430692393627804_7089258403426895782_n.jpg?stp=dst-jpg_p100x100&_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeG7ztbAylG1EyV1_UXH8HyoLsM3d7qxNiAuwzd3urE2IObxzoQ7bkUiSo0Btdi3G45IyOY3PjehkPy187KWTzUI&_nc_ohc=mTdJwZaqlisAb5E4R9X&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.fcrk1-4.fna&oh=00_AfC8XmkvEiBKoXjCPs-Niuk4kd0yX9v3-Un1jzKNGag-IA&oe=66188439",
+    personConversation: "",
+    user: "https://scontent.fcrk1-2.fna.fbcdn.net/v/t39.30808-1/423672175_3699153610408415_4085387455801009384_n.jpg?stp=cp0_dst-jpg_p86x86&_nc_cat=110&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeE_ol_DZaqAWNxJ5BfaaPWI77iDPC4fWS_vuIM8Lh9ZL65BciG9FD3fTasWn-XkrZWk3Ag6X2DGMbzZNj9MuUuv&_nc_ohc=uMkzMc-9CP0Ab5vZLAt&_nc_ht=scontent.fcrk1-2.fna&oh=00_AfArx3DBDHLDqNCuap3Bj4m27GJT5aUHB4-0saMkB_4OSA&oe=661883D0",
+    userConversation: "Oo naman",
+  },
+  {
+    id: 2,
+    personProfile:
+      "https://scontent.fcrk1-4.fna.fbcdn.net/v/t39.30808-1/342805416_6430692393627804_7089258403426895782_n.jpg?stp=dst-jpg_p100x100&_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeG7ztbAylG1EyV1_UXH8HyoLsM3d7qxNiAuwzd3urE2IObxzoQ7bkUiSo0Btdi3G45IyOY3PjehkPy187KWTzUI&_nc_ohc=mTdJwZaqlisAb5E4R9X&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.fcrk1-4.fna&oh=00_AfC8XmkvEiBKoXjCPs-Niuk4kd0yX9v3-Un1jzKNGag-IA&oe=66188439",
+    personConversation: " Jap Pogi mo talaga",
+    user: "https://scontent.fcrk1-2.fna.fbcdn.net/v/t39.30808-1/423672175_3699153610408415_4085387455801009384_n.jpg?stp=cp0_dst-jpg_p86x86&_nc_cat=110&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeE_ol_DZaqAWNxJ5BfaaPWI77iDPC4fWS_vuIM8Lh9ZL65BciG9FD3fTasWn-XkrZWk3Ag6X2DGMbzZNj9MuUuv&_nc_ohc=uMkzMc-9CP0Ab5vZLAt&_nc_ht=scontent.fcrk1-2.fna&oh=00_AfArx3DBDHLDqNCuap3Bj4m27GJT5aUHB4-0saMkB_4OSA&oe=661883D0",
+    userConversation: "hello",
+  },
+  {
+    id: 3,
+    personProfile:
+      "https://scontent.fcrk1-4.fna.fbcdn.net/v/t39.30808-1/342805416_6430692393627804_7089258403426895782_n.jpg?stp=dst-jpg_p100x100&_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeG7ztbAylG1EyV1_UXH8HyoLsM3d7qxNiAuwzd3urE2IObxzoQ7bkUiSo0Btdi3G45IyOY3PjehkPy187KWTzUI&_nc_ohc=mTdJwZaqlisAb5E4R9X&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.fcrk1-4.fna&oh=00_AfC8XmkvEiBKoXjCPs-Niuk4kd0yX9v3-Un1jzKNGag-IA&oe=66188439",
+    personConversation: "",
+    user: "https://scontent.fcrk1-2.fna.fbcdn.net/v/t39.30808-1/423672175_3699153610408415_4085387455801009384_n.jpg?stp=cp0_dst-jpg_p86x86&_nc_cat=110&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeE_ol_DZaqAWNxJ5BfaaPWI77iDPC4fWS_vuIM8Lh9ZL65BciG9FD3fTasWn-XkrZWk3Ag6X2DGMbzZNj9MuUuv&_nc_ohc=uMkzMc-9CP0Ab5vZLAt&_nc_ht=scontent.fcrk1-2.fna&oh=00_AfArx3DBDHLDqNCuap3Bj4m27GJT5aUHB4-0saMkB_4OSA&oe=661883D0",
+    userConversation: "hello",
+  },
+  {
+    id: 4,
+    personProfile:
+      "https://scontent.fcrk1-4.fna.fbcdn.net/v/t39.30808-1/342805416_6430692393627804_7089258403426895782_n.jpg?stp=dst-jpg_p100x100&_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeG7ztbAylG1EyV1_UXH8HyoLsM3d7qxNiAuwzd3urE2IObxzoQ7bkUiSo0Btdi3G45IyOY3PjehkPy187KWTzUI&_nc_ohc=mTdJwZaqlisAb5E4R9X&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.fcrk1-4.fna&oh=00_AfC8XmkvEiBKoXjCPs-Niuk4kd0yX9v3-Un1jzKNGag-IA&oe=66188439",
+    personConversation: "",
+    user: "https://scontent.fcrk1-2.fna.fbcdn.net/v/t39.30808-1/423672175_3699153610408415_4085387455801009384_n.jpg?stp=cp0_dst-jpg_p86x86&_nc_cat=110&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeE_ol_DZaqAWNxJ5BfaaPWI77iDPC4fWS_vuIM8Lh9ZL65BciG9FD3fTasWn-XkrZWk3Ag6X2DGMbzZNj9MuUuv&_nc_ohc=uMkzMc-9CP0Ab5vZLAt&_nc_ht=scontent.fcrk1-2.fna&oh=00_AfArx3DBDHLDqNCuap3Bj4m27GJT5aUHB4-0saMkB_4OSA&oe=661883D0",
+    userConversation: "hello",
+  },
+
+];
+
+export const messages = [
+  {
+    id: 1,
+    name: "Adrian Barrios",
+    nickname: "Underdog",
+    profile:
+      "https://scontent.fcrk1-4.fna.fbcdn.net/v/t39.30808-1/342805416_6430692393627804_7089258403426895782_n.jpg?stp=dst-jpg_p100x100&_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeG7ztbAylG1EyV1_UXH8HyoLsM3d7qxNiAuwzd3urE2IObxzoQ7bkUiSo0Btdi3G45IyOY3PjehkPy187KWTzUI&_nc_ohc=CrwY1611DHEAb5BXT9H&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.fcrk1-4.fna&oh=00_AfC1f3-35BZJGcEsCXLsTr1ekepY9Hz1S9Y4EvRrjTvbNg&oe=66176AF9",
+    chat: "Dami mo nangang electricfan",
+    // more
+  },
+  {
+    id: 2,
+    name: "Jobert Dela Cruz",
+    nickname: "Jobert Dela Cruz",
+    profile:
+      "https://scontent.fcrk1-5.fna.fbcdn.net/v/t39.30808-1/433144291_2111893395837094_5241777717003793999_n.jpg?stp=dst-jpg_p100x100&_nc_cat=104&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeHXrdOIQi8U1LvDkzXA6JyG7ABShiAUAWvsAFKGIBQBa1eDMolX6Z7JaAw10L_rvs_OR8ydxx1WMvqpuaS2TOx2&_nc_ohc=g8P7FkBul2YAb7udk-X&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.fcrk1-5.fna&oh=00_AfBXgUuR6toBVyrwiEv31XIFAwuVWVHLNzCoSr1GpMP65w&oe=661777A1",
+    chat: "Gago HAHHAH YUNG MUKA KO DYAN 😭",
+    // more
+  },
+  {
+    id: 3,
+    name: "Jobert Dela Cruz",
+    nickname: "Jobert Dela Cruz",
+    profile:
+      "https://scontent.fcrk1-5.fna.fbcdn.net/v/t39.30808-1/433144291_2111893395837094_5241777717003793999_n.jpg?stp=dst-jpg_p100x100&_nc_cat=104&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeHXrdOIQi8U1LvDkzXA6JyG7ABShiAUAWvsAFKGIBQBa1eDMolX6Z7JaAw10L_rvs_OR8ydxx1WMvqpuaS2TOx2&_nc_ohc=g8P7FkBul2YAb7udk-X&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.fcrk1-5.fna&oh=00_AfBXgUuR6toBVyrwiEv31XIFAwuVWVHLNzCoSr1GpMP65w&oe=661777A1",
+    chat: "Gago HAHHAH YUNG MUKA KO DYAN 😭",
+    // more
+  },
+ 
+]; 
+
